d3ae6a0bc51186fb66b1ada82bd2e7c907baa9e5
OssiKauppinen <ossi.kauppinen@edu.salpaus.fi>
2020-11-23 08:59:14 +0000

    20201123

---
+++ scriptemail.js
@@ -0,0 +1,30 @@
+//console.log("Scripti toimii");
+
+const emailnappi = document.querySelector('.lahetanappi');
+
+emailnappi.addEventListener('click', e => {
+  e.preventDefault();
+  sendJSON();
+});
+
+function sendJSON(){
+  let xhr = new XMLHttpRequest();
+  let url = "https://salpausemail.azurewebsites.net/api/HttpTriggerCSharp1?code=lWOELqiU07AqsBviOQYzuNIrQP7xoV7NV7C5W2ctgjIRcf7nXE2biw==";
+
+  xhr.open("POST", url, true);
+  
+  xhr.setRequestHeader("Content-type", "application/json");
+
+  xhr.onreadystatechange = function(){
+    if(xhr.readyState === 4 && xhr.satus === 200){
+      console.log("Valmis, yhteys toimii");
+    }
+  };
+  var data = JSON.stringify({
+    "EmailMsg": "Tähän tulee postin sisältö", // Kirjoittaa spostin sisällön
+    "EmailAddress": "mira.vorne@salpaus.fi", // Viestin kirjoittajan sposti
+    "EmailTo": "ossi.kauppinen@edu.salpaus.fi", // Oma spostini
+    "EmailName": "Teppo Tyyppi" // Nimikentän sisältö
+  });
+  xhr.send(data);
+}
