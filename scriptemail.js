7565be3d8d341ac2375a84bac566cb1fcf79895f
OssiKauppinen <ossi.kauppinen@edu.salpaus.fi>
2020-11-23 10:24:59 +0000

    20201123

--- scriptemail.js
+++ scriptemail.js
@@ -25,10 +25,51 @@ function sendJSON(){
     }
   };
   var data = JSON.stringify({
+    "EmailMsg": "Viestin sisältö", // Kirjoittaa spostin sisällön
+    "EmailAddress": "ossi.kauppinen@salpaus.fi", // Viestin kirjoittajan sposti
+    "EmailTo": "ossi.o.kauppinen@gmail.com", // Oma spostini
+    "EmailName": "ossi" // Nimikentän sisältö
+  });
+  xhr.send(data);
+};
+
+
+
+
+
+/*
+var data = JSON.stringify({
     "EmailMsg": `${viestiKentta}`, // Kirjoittaa spostin sisällön
     "EmailAddress": `${emailKentta}`, // Viestin kirjoittajan sposti
     "EmailTo": "ossi.o.kauppinen@gmail.com", // Oma spostini
     "EmailName": `${nimiKentta}` // Nimikentän sisältö
   });
   xhr.send(data);
-}
+};
+
+const lahetysnappi = document.querySelector('.lahetanappi');
+const nimiSisalto = document.querySelector('#nimi');
+const postiSisalto = document.querySelector('#email');
+const virhe = document.querySelector('.virheilmoitus');
+const kiitos = document.querySelector('.kiitosilmoitus');
+
+lahetysnappi.addEventListener('click', e =>{
+  e.preventDefault();
+
+  if(nimiSisalto.value === '' || postiSisalto.value === ''){
+    virhe.classList.add('virhe');
+    virhe.innerHTML = 'Täytä kaikki pakolliset kentät';
+    nimiSisalto.style.borderColor = 'red';
+    postiSisalto.style.borderColor = 'red';
+  }else{
+  kiitos.innerHTML = (`Kiitos yhteydenotostasi ${nimiSisalto.value}`);
+  setTimeout(() => kiitos.innerHTML = "", 5000);
+  setTimeout(() => kiitos.classList.remove('kiitos'), 5000);
+  postiSisalto.value = '';
+  virhe.classList.remove('virhe');
+  virhe.innerHTML = '';
+  kiitos.classList.add('kiitos');
+  nimiSisalto.style.borderColor = 'black';
+  postiSisalto.style.borderColor = 'black';
+  lahetysnappi.value = 'Lähetä';
+}});*/
