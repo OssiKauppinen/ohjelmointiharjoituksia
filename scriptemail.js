041a1800a01cc87453c146267a16d77ebaf77283
OssiKauppinen <ossi.kauppinen@edu.salpaus.fi>
2020-11-23 10:07:21 +0000

    20201123

--- scriptemail.js
+++ scriptemail.js
@@ -7,6 +7,10 @@ emailnappi.addEventListener('click', e => {
   sendJSON();
 });
 
+const nimiKentta = document.querySelector('#nimi')
+const emailKentta = document.querySelector('#email')
+const viestiKentta = document.querySelector('textarea')
+
 function sendJSON(){
   let xhr = new XMLHttpRequest();
   let url = "https://salpausemail.azurewebsites.net/api/HttpTriggerCSharp1?code=lWOELqiU07AqsBviOQYzuNIrQP7xoV7NV7C5W2ctgjIRcf7nXE2biw==";
@@ -21,10 +25,10 @@ function sendJSON(){
     }
   };
   var data = JSON.stringify({
-    "EmailMsg": "Tähän tulee postin sisältö", // Kirjoittaa spostin sisällön
-    "EmailAddress": "mira.vorne@salpaus.fi", // Viestin kirjoittajan sposti
+    "EmailMsg": `${viestiKentta}`, // Kirjoittaa spostin sisällön
+    "EmailAddress": `${emailKentta}`, // Viestin kirjoittajan sposti
     "EmailTo": "ossi.o.kauppinen@gmail.com", // Oma spostini
-    "EmailName": "Teppo Tyyppi" // Nimikentän sisältö
+    "EmailName": `${viestiKentta}` // Nimikentän sisältö
   });
   xhr.send(data);
 }
