6972898a38fbb76a41601a6f5e214241f795852e
OssiKauppinen <ossi.kauppinen@edu.salpaus.fi>
2020-12-07 09:09:40 +0000

    20201207

--- historia.js
+++ historia.js
@@ -18,11 +18,26 @@ var getJSON = function(url, callback) {
 };
 
 getJSON('https://api.apify.com/v2/datasets/BDEAOLx0DzEW91s5L/items?format=json&clean=1', function(err, data) {
+let taulukko = '<table style="border:1px solid black">'
+const historia = data.map(function(paiva){
 
-  if (err != null) {
-    console.error(err);
-  } else{
-    console.log(data.confirmedCases);
-    console.log(data.lastUpdatedAtSource);
+  if(paiva.confirmedCases != undefined){
+    console.log(`Confirmed: ${paiva.confirmedCases}`)
+    taulukko = taulukko + `<tr><td>Confirmed</td></tr>${paiva.confirmedCases}</td></tr>`;
   }
+  if(paiva.tested != undefined){
+    console.log(`Tested: ${paiva.testedCases}`)
+    taulukko = taulukko + `<tr><td>Tested</td></tr>${paiva.testedCases}</td></tr>`;
+  }
+  if(paiva.infected != undefined){
+    console.log(`Infected: ${paiva.infected}`)
+    taulukko = taulukko + `<tr><td>Infected</td></tr>${paiva.infected}</td></tr>`;
+  }
+  else{
+    console.log('Ei dataa');
+    taulukko = taulukko + '<tr><td>Ei dataa</td><td>Ei dataa</td></td>'
+  }
+});
+taulukko = taulukko + '</table>'
+document.body.innerHTML=taulukko;
 });
